83049e641eeebada3d1bb4a711e1de23623d3b60
Ozan0062 <ozan0062@edu.zealand.dk>
2024-11-14 15:28:14 +0100

    Har fjernet Secrets klassen og har ændret gitignore filen. Jeg tror det burde virke når det her bliver merget og når jeg tilføjer secrets klassen igen

--- Secrets.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Skal_vi_videre
-{
-    public static class Secrets
-    {
-        public static string ConnectionString = ("Data Source=mssql4.unoeuro.com;Initial Catalog=skalvividere_nu_db_svv_db;Persist Security Info=True;User ID=skalvividere_nu;Password=bpanGcHt4m6ygE52hRxB");
-    }
-}
