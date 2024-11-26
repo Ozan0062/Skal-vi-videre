fc2640b5dc658d86ab91ca62d94e1c18cda4a7cd
Ozan0062 <ozan0062@edu.zealand.dk>
2024-11-26 15:40:41 +0100

    Har lavet en Repository til Company som arver fra BaseRepository, men overrider Create metoden så adgangskoden kan hashes og der er også blevet tilføjet en VerifyCompany metode som tjekker om email og Password er rigtig. GetByCvr metode er blevet lavet så man kan finde virksomheder ved hjælp af Cvr nummeret.

---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Skal_vi_videre.Repository.Base;
+
+namespace Skal_vi_videre.Repository
+{
+    public class CompanyRepository : BaseRepository<Company>
+    {
+        private PasswordHasher<string> _passwordHasher = new PasswordHasher<string>();
+
+        public override int Create(Company company)
+        {
+            company.Password = _passwordHasher.HashPassword(company.Email, company.Password);
+            return base.Create(company);
+        }
+
+        public Company? GetByCvr(string cvrId)
+        {
+            using DbContext context = CreateDbContext();
+            Company? result = GetAllWithIncludes(context).FirstOrDefault(t => t.Cvr == cvrId);
+
+            return result;
+        }
+
+        public Company? VerifyCompany(string providedEmail, string providedPassword)
+        {
+            Company? company = GetAll().FirstOrDefault(u => u.Email == providedEmail);
+
+            if (company == null ||
+            _passwordHasher.VerifyHashedPassword(
+                    providedEmail,
+                    company.Password,
+                    providedPassword)
+                != PasswordVerificationResult.Success)
+                return null;
+
+            return company;
+        }
+    }
+}
