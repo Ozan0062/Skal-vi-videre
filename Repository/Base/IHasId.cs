7c83b5629b4944d57c8ca869c46c3d8ed5a53372
Ozan0062 <ozan0062@edu.zealand.dk>
2024-11-26 15:34:50 +0100

    Har lavet en Base repository med 3 Interfaces som er IHasId, IRepository og IUpdateFromOther. Dem har jeg lavet en ny mappe som jeg har kaldt Repository og har lavet en anden mappe inde i den som jeg har kaldet Base. BaseRepository klassen arver fra IRepository som indeholder  GetAll, Create, Update og Delete og Repositorien indeholder også det samme. Den arver også fra IHasId som indeholder Id og bruges for at håndter id så de er ensartet. Den arver også fra IUpdateFromOther som indeholder Update og bruges bruges til at definere en regel for, hvordan et objekt kan opdatere sig selv baseret på et andet objekt af samme type.

---
+++ Repository/Base/IHasId.cs
@@ -0,0 +1,7 @@
+﻿namespace Skal_vi_videre.Repository.Base
+{
+    public interface IHasId
+    {
+        int Id { get; set; }
+    }
+}
---
+++ Repository/Base/IUpdateFromOther.cs
@@ -0,0 +1,7 @@
+﻿namespace Skal_vi_videre.Repository.Base
+{
+    public interface IUpdateFromOther<T>
+    {
+        void Update(T tOther);
+    }
+}
