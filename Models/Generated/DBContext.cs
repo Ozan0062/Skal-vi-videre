d81d401c8dfa596c9ce1102dd8c5eab24bb7567c
Ozan0062 <ozan0062@edu.zealand.dk>
2024-12-02 11:56:33 +0100

    Har tilføjet secrets i dbcontext

--- Models/Generated/DBContext.cs
+++ Models/Generated/DBContext.cs
@@ -23,7 +23,7 @@ public partial class DBContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=mssql4.unoeuro.com;Initial Catalog=skalvividere_nu_db_svv_db;Persist Security Info=True;User ID=skalvividere_nu;Password=bpanGcHt4m6ygE52hRxB;Encrypt=True");
+        => optionsBuilder.UseSqlServer(Secrets.ConnectionString);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
