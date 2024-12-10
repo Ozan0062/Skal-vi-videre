60e3dc55ee40672f1255f47985c81ace51fc6708
Ozan0062 <ozan0062@edu.zealand.dk>
2024-12-10 10:55:38 +0100

    Har lavet en githubsecrets så github actions kan virke.

---
+++ GithubSecrets.cs
@@ -0,0 +1,12 @@
+﻿namespace Skal_vi_videre
+{
+    public static class GithubSecrets
+    {
+        // Hvis miljøvariablen "GITHUB_ACTIONS" er sat, så er vi i GitHub Actions, ellers er vi i Visual Studio.
+        public static string ConnectionString =
+            Environment.GetEnvironmentVariable("GITHUB_ACTIONS") == null ? Secrets.ConnectionString : "";
+
+        public static string APIKey =
+            Environment.GetEnvironmentVariable("GITHUB_ACTIONS") == null ? Secrets.APIKey : "";
+    }
+}
