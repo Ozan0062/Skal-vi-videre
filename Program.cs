6f79f7670fcb3161771b60562ac3966ae26b7537
Ozan0062 <ozan0062@edu.zealand.dk>
2024-12-19 16:25:45 +0100

    Dev v2

--- Program.cs
+++ Program.cs
@@ -58,7 +58,6 @@ app.UseSwaggerUI();
 
 app.UseCors();
 
-// Configure the HTTP request pipeline.
 app.UseExceptionHandler("/Error", createScopeForErrors: true);
 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 app.UseHsts();
